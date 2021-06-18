f451d9a3e34e8bf30e99ec79b36cb04a83c33c7e
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-18 13:19:28 +0200

    add basic views and urls

--- Project_InTheRightHands/settings.py
+++ Project_InTheRightHands/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Charity_app',
+    'Project_InTheRightHands',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'Project_InTheRightHands.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 try:
-    from Project_InTheRightHands.local_settings import DATABASES
+    from local_settings import DATABASES
 except ModuleNotFoundError:
     print("Database is not configured in local_settings.py!")
     print("Fill the missing file and try again!")
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
