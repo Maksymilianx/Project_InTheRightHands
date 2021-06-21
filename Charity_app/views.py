d0acb8dd0f65d6216c796fd7ee799445ca344e05
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-21 20:06:48 +0200

    add views

--- Charity_app/views.py
+++ Charity_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+from Charity_app.models import Donation, Institutions
+
 
 class LandingPage(View):
     def get(self, request):
@@ -20,3 +22,15 @@ class Login(View):
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+
+
+class CountBags(View):
+    def get(self, request):
+        queryset = list(Donation.objects.all())
+        return render(request, "base.html", {'queryset': queryset})
+
+
+class CountOrganizations:
+    def get(self, request):
+        lst = list(Institutions.objects.all())
+        return render(request, "base.html", {'lst': lst})
