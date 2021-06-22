db4dbc9607c4b0e08abcf2a6665059423356ab59
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-22 22:18:49 +0200

    add anchor links

--- Charity_app/views.py
+++ Charity_app/views.py
@@ -26,11 +26,11 @@ class Register(View):
 
 class CountBags(View):
     def get(self, request):
-        queryset = list(Donation.objects.all())
-        return render(request, "base.html", {'queryset': queryset})
+        bags = Donation.objects.filter('quantity')
+        return render(request, "base.html", {'bags': bags})
 
 
 class CountOrganizations:
     def get(self, request):
-        lst = list(Institutions.objects.all())
-        return render(request, "base.html", {'lst': lst})
+        organizations = Institutions.objects.filter('name')
+        return render(request, "base.html", {'organizations': organizations})
